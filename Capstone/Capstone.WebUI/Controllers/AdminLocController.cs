d4db62e71a56c5cde9f35856c90bf0629be63fd4
Marian <katsrneet@gmail.com>
2014-04-28 20:47:15 -0700

    Added Controllers
    Added controllers for user and location

---
+++ Capstone/Capstone.WebUI/Controllers/AdminLocController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.WebUI.Controllers
+{
+    public class AdminLocController : Controller
+    {
+        //
+        // GET: /AdminLoc/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
