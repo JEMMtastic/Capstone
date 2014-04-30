0c3851740be356e7fba6929c5ebab0f3bc375d95
Marian <katsrneet@gmail.com>
2014-04-30 10:18:00 -0700

    Some stuff
    Lunch work on my controllers

--- Capstone/Capstone.WebUI/Controllers/AdminUserController.cs
+++ Capstone/Capstone.WebUI/Controllers/AdminUserController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Capstone.Domain.Abstract;
+using Capstone.Domain.Concrete;
+using Capstone.Domain.Entities;
 
 namespace Capstone.WebUI.Controllers
 {
     public class AdminUserController : Controller
-    {
-        //
-        // GET: /AdminUser/
+    { 
+        public ActionResult AdminUserIndex()
+        {
+            //need to get a list of all users
+            var db = new CapstoneDbContext();
+            List<User> users = (from u in db.Users
+                                select u).ToList<User>();
 
-        public ActionResult Index()
+               
+            //TODO:  add functionality to get users by restaurant or city?
+            return View(users);
+        }
+
+        public ActionResult AddUser()
         {
             return View();
         }
