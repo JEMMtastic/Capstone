a0e273c96dc32b796256c636d88d277c12172d5a
mpolicicchio <aliquisme@gmail.com>
2014-05-02 08:51:23 -0700

    StatsInfo [Required(ErrorMessage)], [HiddenInput]
    I entered the [Required(ErrorMessage)]s to all properties in the
    StatsInfo class. I also added [HiddenInput] to the id property.

--- Capstone/Capstone.Domain/Entities/StatsInfo.cs
+++ Capstone/Capstone.Domain/Entities/StatsInfo.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Capstone.Domain.Entities
 {
     public class StatsInfo
     {
+        [HiddenInput(DisplayValue = false)]
         public int StatsInfoId { get; set; }
+
+        [Required(ErrorMessage = "Please enter the total sales.")]
         public decimal TotalSales { get; set; }
+
+        [Required(ErrorMessage = "Please enter the amount of total sales to charity.")]
         public decimal AmountOfTotalSalesToCharity { get; set; }
+
+        [Required(ErrorMessage = "Please enter the cash donations.")]
         public decimal CashDonations { get; set; }
+
+        [Required(ErrorMessage = "Please enter the guest count.")]
         public int GuestCount { get; set; }
+
+        [Required(ErrorMessage = "Please enter the partnership night.")]
         public PartnershipNight partnershipNight { get; set; }
     }
 }
