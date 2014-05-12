4ab7d3c46cedc789937930482df2334fb9781d09
Marian <katsrneet@gmail.com>
2014-05-11 18:39:59 -0700

    requireds added
    addes requireds for section 0 and 1

--- Capstone/Capstone.Domain/Entities/Section01.cs
+++ Capstone/Capstone.Domain/Entities/Section01.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,25 +17,45 @@ namespace Capstone.Domain.Entities
         //telephone # to come from charity data
         //fed tax id to come from charity data
         //bv store num to come from bvlocation attached to pn
+        [Required(ErrorMessage="You must fill out all fields")]
         public int Wk1FourGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk1FiveGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk1SixGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk1SevenGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk1EightGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk2FourGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk2FiveGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk2SixGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int WkSevenGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk2EightGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk3FourGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk3FiveGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk3SixGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk3SevenGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public int Wk3EightGc { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public decimal AvgChk4 { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public decimal AvgChk5 { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public decimal AvgChk6 { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public decimal AvgChk7 { get; set; }
+        [Required(ErrorMessage = "You must fill out all fields")]
         public decimal AVgChk8 { get; set; }
     }
 }
